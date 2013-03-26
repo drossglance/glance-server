7a1b10cb798627386f1ce34913cc36989b91a73e
Victor Basso <victor.basso@frequency.mygbiz.com>
2013-03-26 19:14:01 +0000

    -

--- src/main/java/uk/frequency/glance/server/model/event/EventType.java
+++ src/main/java/uk/frequency/glance/server/model/event/EventType.java
@@ -1,5 +1,5 @@
 package uk.frequency.glance.server.model.event;
 
 public enum EventType {
-	STAY, WORK, PUB, EXERCISE, SLEEP, WAKE, WALK, CYCLING, COMUTE, TRAVEL, MEETING, MUSIC, MOVIE // TODO this list is not definite
+	STAY, HOME, WORK, PUB, EXERCISE, SLEEP, WAKE, WALK, CYCLING, COMUTE, TRAVEL, MEETING, MUSIC, MOVIE // TODO this list is not definite
 } 
